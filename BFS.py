d27e67480ab1c2c386895c187b7269880fdc2a95
mansisaini9193 <102938293+mansisaini9193@users.noreply.github.com>
2022-06-16 20:35:50 +0530

    Create BFS.py

---
+++ BFS.py
@@ -0,0 +1,27 @@
+
+
+import collections
+
+
+def bfs(g, root):
+
+    visited, queue = set(), collections.deque([root])
+    visited.add(root)
+
+    while queue:
+
+        
+        vertex = queue.popleft()
+        print(str(vertex) + " ", end="")
+
+        
+        for neighbour in g[vertex]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+
+
+if __name__ == '__main__':
+    g = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
+    print("Following is Breadth First Traversal: ")
+    bfs(g, 0)
